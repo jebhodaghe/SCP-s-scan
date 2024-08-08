e43ea79e4e1a8ecfdb007c870c668d6b33632fe0
jebhodaghe <jebhodaghe@libertyglobal.com>
2024-08-08 10:51:54 +0100

    Update custom_checkov_rule.py

--- custom_checkov_rule.py
+++ custom_checkov_rule.py
@@ -4,18 +4,19 @@ from checkov.json_doc.registry import registry
 
 class CustomSCPCheck(BaseJsonCheck):
     def __init__(self):
-        name = "Ensure CloudTrail is configured correctly in SCP"
-        id = "CUSTOM_SCP_1"
+        name = 'Ensure CloudTrail is configured correctly in SCP'
+        id = 'CUSTOM_SCP_1'
         supported_entities = ['Statement']
         categories = [CheckCategories.IAM]
+        block_type = 'json'
         super().__init__(
-            name=name, id=id, categories=categories, supported_entities=supported_entities
+            name=name, id=id, categories=categories, supported_entities=supported_entities, block_type=block_type
         )
 
     def scan_entity_conf(self, conf, entity_type):
-        if "Sid" in conf:
-            if conf["Sid"] == "RequireMultiRegionTrailWithReadAndWriteManagementEvents":
-                if not (conf["Condition"].get("StringEquals") and "cloudtrail:ManagementEventReadWriteType" in conf["Condition"]["StringEquals"] and conf["Condition"]["StringEquals"]["cloudtrail:ManagementEventReadWriteType"] == ["All"]):
+        if 'Sid' in conf:
+            if conf['Sid'] == 'RequireMultiRegionTrailWithReadAndWriteManagementEvents':
+                if not (conf['Condition'].get('StringEquals') and 'cloudtrail:ManagementEventReadWriteType' in conf['Condition']['StringEquals'] and conf['Condition']['StringEquals']['cloudtrail:ManagementEventReadWriteType'] == ['All']):
                     return CheckResult.FAILED
         return CheckResult.PASSED
 
