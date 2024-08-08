efc1e4b52112ac7e0c54f61b12f8034bdba02186
jebhodaghe <jebhodaghe@libertyglobal.com>
2024-08-08 10:45:33 +0100

    Create custom_checkov_rule.py

---
+++ custom_checkov_rule.py
@@ -0,0 +1,22 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.json_doc.base_json_check import BaseJsonCheck
+from checkov.json_doc.registry import registry
+
+class CustomSCPCheck(BaseJsonCheck):
+    def __init__(self):
+        name = "Ensure CloudTrail is configured correctly in SCP"
+        id = "CUSTOM_SCP_1"
+        supported_entities = ['Statement']
+        categories = [CheckCategories.IAM]
+        super().__init__(
+            name=name, id=id, categories=categories, supported_entities=supported_entities
+        )
+
+    def scan_entity_conf(self, conf, entity_type):
+        if "Sid" in conf:
+            if conf["Sid"] == "RequireMultiRegionTrailWithReadAndWriteManagementEvents":
+                if not (conf["Condition"].get("StringEquals") and "cloudtrail:ManagementEventReadWriteType" in conf["Condition"]["StringEquals"] and conf["Condition"]["StringEquals"]["cloudtrail:ManagementEventReadWriteType"] == ["All"]):
+                    return CheckResult.FAILED
+        return CheckResult.PASSED
+
+registry.register(CustomSCPCheck())
